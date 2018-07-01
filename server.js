30de0a55b04e3e438879bc39f203b37a56cc9046
Nando <redchameleonapps@gmail.com>
2018-07-01 01:05:56 -0500

    First commit for final NodeJS project

---
+++ server.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var app = express();
+
+const { Pool } = require("pg");
+
+const connectionString = process.env.DATABASE_URL || "postgres://worldcupuser:fego@localhost:5432/worldcuphistory";
+const pool = new Pool({connectionString: connectionString});
+
+app.set("port", (process.env.PORT || 5000));
+
+app.get("/getWorldCupCountry", getWorldCupCountry)
+
+app.listen(app.get("port"), function() {
+    console.log("Now listening for connections on port: ", app.get("port"));
+});
+
+function getWorldCupCountry(req, res) {
+    console.log("Getting World Cup Country.");
+
+    var result = {id: 2, country: "Russia", year: "1984"};
+
+    var id = req.query.id;
+    console.log("Retrieving World Cup with id: ", id);
+
+
+    getWorldCupCountryFromDb(id, function(error, result) {
+        console.log("Got data back from DB with result:", result);
+        if (error || result == null || result.length != 1) {
+            res.status(500).json({success:false, data: error});
+        } else {
+            res.json(result[0]);
+        }
+    });
+}
+
+function getWorldCupCountryFromDb(id, callback) {
+    console.log("getting country from DB with id ", id);
+
+    var sql = "SELECT id, country, year FROM worldcup WHERE id = $1::int";
+    var params = [id];
+
+    pool.query(sql, params, function(err, result){
+        if (err) {
+            console.log("An error occurred with the DB");
+            console.log(err);
+            callback(err, null);
+        }
+        console.log("Found Db result: " + JSON.stringify(result.rows));
+
+        callback(null, result.rows);
+    });
+
+}
+
