a252736c3fbfefe9e3627cf2f6b0dcd9dfda1502
Nando <redchameleonapps@gmail.com>
2018-07-22 00:00:07 -0500

    added new file to start building the login form

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.get('/', (req, res) => res.render('/index'));
 
 app.get('/form', function(req, res, next) {
-    res.sendFile('signUp.html');
+    res.sendFile('/signUp.html');
 });
 
 app.get("/getWorldCupCountry", getWorldCupCountry)
